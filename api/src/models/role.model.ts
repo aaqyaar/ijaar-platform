247e8a9dab9976fd394cb59cd3e82cd17246839a
abdizamedmo <abdizamedmo@gmail.com>
2023-10-11 21:24:39 +0300

    Role model added permissions collection

--- api/src/models/role.model.ts
+++ api/src/models/role.model.ts
@@ -1,16 +1,37 @@
 import { Schema, model, models } from 'mongoose';
 
+interface Permission {
+  subject: string;
+  action: string;
+  role: string;
+  reason: string;
+}
+
 interface Role {
-  name: string;
-  permissions: string[];
+  name: 'user' | 'client' | 'agent' | 'superadmin';
+  permissions: Permission[];
 }
 
 const RoleSchema = new Schema(
   {
-    name: { type: String, required: true, enum: ['admin', 'user', 'teacher', 'partner'] },
-    permissions: [String],
+    name: { type: String, required: true, enum: ['user', 'client', 'agent', 'superadmin'] },
+    permissions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Permission',
+      },
+    ],
   },
   { timestamps: true },
 );
 
-export default models.Role || model<Role>('Role', RoleSchema);
+const PermissionSchema = new Schema({
+  subject: { type: String, required: true },
+  action: { type: String, required: true },
+  role: { type: Schema.Types.ObjectId, ref: 'Role' },
+  reason: { type: String, required: true },
+});
+
+export const Permission = models.Permission || model('Permission', PermissionSchema);
+
+export const Role = models.Role || model<Role>('Role', RoleSchema);
